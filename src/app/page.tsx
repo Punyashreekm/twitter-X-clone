5f190657a5a3c9884e686ce0ccea7d7824993e90
punya <punya8144@gmail.com>
2024-12-30 11:39:51 +0530

    ft:left sidebar

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image"
+
+const Homepage = () => {
+  return (
+    <div className=''>
+      <Image src="/general/post.jpeg" alt=""height={600} width={600}/>
+    </div>
+  )
+}
+
+export default Homepage
